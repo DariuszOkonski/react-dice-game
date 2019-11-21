e47179094986fc65f133bdb86a6805c4339a629a
Dariusz Okonski <darek200180@gmail.com>
2019-11-21 16:11:41 +0000

    add styles.dice

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../css/Dice.module.css';
 
 const Dice = ({ number }) => {
   return (
-    <div>
+    <div className={styles.dice}>
       <i className={`fas fa-dice-${number}`}></i>
     </div>
   );
