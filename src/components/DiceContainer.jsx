59b70c2817eb73c6addd38dda966643396f31d65
Dariusz Okonski <darek200180@gmail.com>
2019-11-21 16:12:38 +0000

    display static four Dices

--- src/components/DiceContainer.jsx
+++ src/components/DiceContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Dice from './Dice';
+import styles from '../css/DiceContainer.module.css';
 
 const DiceContainer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Dice number="one" />
       <Dice number="two" />
+      <Dice number="three" />
+      <Dice number="four" />
     </div>
   );
 }
