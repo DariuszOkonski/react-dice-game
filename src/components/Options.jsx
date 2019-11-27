1bda4ec9b0d89c40211c903f941dded9c2a3b0fe
Dariusz Okonski <darek200180@gmail.com>
2019-11-27 10:26:51 +0000

    change path

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -6,10 +6,10 @@ import { Link } from 'react-router-dom';
 const Options = () => {
   return (
     <div className={styles.wrapper}>
-      <Link className={buttonStyles.button} to='/1'>1 Dice Game</Link>
-      <Link className={buttonStyles.button} to='/2'>2 Dices Game</Link>
-      <Link className={buttonStyles.button} to='/3'>3 Dices Game</Link>
-      <Link className={buttonStyles.button} to='/4'>4 Dices Game</Link>
+      <Link className={buttonStyles.button} to='/game1'>1 Dice Game</Link>
+      <Link className={buttonStyles.button} to='/game2'>2 Dices Game</Link>
+      <Link className={buttonStyles.button} to='/game3'>3 Dices Game</Link>
+      <Link className={buttonStyles.button} to='/game4'>4 Dices Game</Link>
     </div>
   );
 }
