e093637604bffd6e512f3d2369a69558516e8715
Dariusz Okonski <darek200180@gmail.com>
2019-11-27 10:18:32 +0000

    add routing to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import Game from './Game';
 import styles from '../css/App.module.css';
 import Options from './Options';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Error from './Error';
 
 
 function App() {
   return (
-    <div className={styles.container}>
-      <Game />
-      <Options />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className={styles.container}>
+        <Switch>
+          <Route exact path='/' component={Options} />
+          <Route exact path='/1' component={Game} />
+          <Route exact path='/2' component={Game} />
+          <Route exact path='/3' component={Game} />
+          <Route exact path='/4' component={Game} />
+          <Route component={Error} />
+        </Switch>
+
+
+      </div>
+    </BrowserRouter>
   );
 }
 
