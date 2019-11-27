f97f8cb22cfdd60ebace3ddaeedca851f2703528
Dariusz Okonski <darek200180@gmail.com>
2019-11-27 10:16:19 +0000

    change Button to Link and add buttonsStyles

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import Button from './Button';
 import styles from '../css/Game.module.css';
+import buttonStyles from '../css/Button.module.css';
+import { Link } from 'react-router-dom';
 
 const Options = () => {
   return (
     <div className={styles.wrapper}>
-      <Button>1 Dice Game</Button>
-      <Button>2 Dices Game</Button>
-      <Button>3 Dices Game</Button>
-      <Button>4 Dices Game</Button>
+      <Link className={buttonStyles.button} to='/1'>1 Dice Game</Link>
+      <Link className={buttonStyles.button} to='/2'>2 Dices Game</Link>
+      <Link className={buttonStyles.button} to='/3'>3 Dices Game</Link>
+      <Link className={buttonStyles.button} to='/4'>4 Dices Game</Link>
     </div>
   );
 }
